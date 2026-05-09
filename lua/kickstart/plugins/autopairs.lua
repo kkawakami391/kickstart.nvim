16b6cb569faa4a9c628214aa8c521c4ddb13f929
kkawakami391 <kkawakami391@gmail.com>
2026-05-09 12:36:23 +0900

    Commented out for unwanted behavior

--- lua/kickstart/plugins/autopairs.lua
+++ lua/kickstart/plugins/autopairs.lua
@@ -11,8 +11,8 @@ return {
       check_ts = true,
     }
     -- If you want to automatically add `(` after selecting a function or method
-    local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
-    local cmp = require 'cmp'
-    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    -- local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+    -- local cmp = require 'cmp'
+    -- cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
   end,
 }
